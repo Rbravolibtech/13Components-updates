45b5f132a81eaf885afb5407b205a57b763ea730
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-10-03 15:36:33 -0600

    using callBack and it dependencies

--- src/App.js
+++ src/App.js
@@ -5,18 +5,27 @@ import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
 	const [showParagraph, setShowParagraph] = useState(false);
+	const [allowToggle, setAllowParagraph] = useState(false);
 
 	console.log("APP RUNNING");
 
 	const clickHereHandler = useCallback(() => {
+		if (allowToggle) {
+			setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+		}
 		setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-	}, []);
+	}, [allowToggle]);
+
+	const clickHereNowHandle = () => {
+		setAllowParagraph(true);
+	};
 
 	return (
 		<div className="app">
 			<h1>Hi there!</h1>
-			<DemoOutput show={false} />
+			<DemoOutput show={showParagraph} />
 			<Button onClick={clickHereHandler}>CLICK HERE!</Button>
+			<Button onClick={clickHereNowHandle}>CLICK HERE NOW !</Button>
 		</div>
 	);
 }
